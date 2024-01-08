617f5e3d417f8064b41b58e64c0b695ee176e328
Luis Dourado <lfaddourado@gmail.com>
2024-01-08 16:14:28 -0300

    feat: include sign out button

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,10 +1,23 @@
-import { auth } from '@/server/auth';
+import { Button } from '@/components/ui/button';
+import { auth, signOut } from '@/server/auth';
 import React from 'react';
 
 async function SettingsPage() {
 	const session = await auth();
 	console.log(session);
-	return <div>SettingsPage</div>;
+	return (
+		<div>
+			SettingsPage
+			<form
+				action={async () => {
+					'use server';
+					await signOut();
+				}}
+			>
+				<Button type="submit">Sair</Button>
+			</form>
+		</div>
+	);
 }
 
 export default SettingsPage;
