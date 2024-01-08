ab87e099bd2395a40d1a96b780ab486f7bb5961b
Luis Dourado <lfaddourado@gmail.com>
2024-01-08 11:48:14 -0300

    feat: add register page

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from '@/components/auth/RegisterForm';
+
+export const RegisterPage = () => {
+	return (
+		<div>
+			<RegisterForm />
+		</div>
+	);
+};
+
+export default RegisterPage;
