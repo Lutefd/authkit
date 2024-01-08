a62c604e4480035a84f61e9ab10e74e188accfa7
Luis Dourado <lfaddourado@gmail.com>
2024-01-08 15:50:44 -0300

    feat; refactor middleware.ts to handle authentication routes and redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,35 @@
 import authConfig from '@/server/auth.config';
 import NextAuth from 'next-auth';
-import { apiAuthPrefix } from './routes';
+import {
+	DEFAULT_LOGIN_REDIRECT,
+	apiAuthPrefix,
+	authenticationRoutes,
+	publicRoutes,
+} from './routes';
 export const { auth } = NextAuth(authConfig);
 export default auth((req) => {
 	const { nextUrl } = req;
 	const isLoggedIn = req.auth;
 	const isApiRoute = req.url.startsWith(apiAuthPrefix);
+	const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+	const isAuthRoute = authenticationRoutes.includes(nextUrl.pathname);
+
+	if (isApiRoute) {
+		return null;
+	}
+
+	if (isAuthRoute) {
+		if (isLoggedIn) {
+			return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+		}
+		return null;
+	}
+
+	if (!isLoggedIn && !isPublicRoute) {
+		return Response.redirect(new URL('/auth/login', nextUrl));
+	}
+
+	return null;
 });
 
 export const config = {
