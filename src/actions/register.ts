42cbcd8dde8bd9925231f490070836581dbadbdf
Luis Dourado <lfaddourado@gmail.com>
2024-01-08 15:05:43 -0300

    feat: add bcrypt password hashing and user registration functionality

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,6 +1,8 @@
 'use server';
 import { RegisterSchema } from '@/schemas';
 import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import { db } from '@/server/db';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
 	const validateFields = RegisterSchema.safeParse(values);
@@ -10,6 +12,29 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 			error: 'Ocorreu um erro ao realizar o cadastro  ',
 		};
 	}
+	const { email, password, name } = validateFields.data;
+
+	const hashPassword = await bcrypt.hash(password, 10);
+
+	const existingUser = await db.user.findUnique({
+		where: {
+			email,
+		},
+	});
+
+	if (existingUser) {
+		return {
+			error: 'Usuário já cadastrado',
+		};
+	}
+
+	await db.user.create({
+		data: {
+			email,
+			password: hashPassword,
+			name,
+		},
+	});
 
 	return {
 		success: 'Cadastro realizado com sucesso',
