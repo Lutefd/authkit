d7de22b5aeaf1bebd00ca41b3351baf2d0f29345
Luis Dourado <lfaddourado@gmail.com>
2024-01-08 15:41:54 -0300

    feat: add utility variables to deal with authentication

---
+++ src/routes.ts
@@ -0,0 +1,26 @@
+/**
+ * Array de rotas que serão acessíveis sem autenticação
+ * @type {string[]}
+ */
+export const publicRoutes = ['/'];
+
+/**
+ * Esse array de rotas será acessível apenas se o usuário estiver autenticado
+ * @type {string[]}
+ */
+
+export const privateRoutes = ['/settings'];
+
+/**
+ * Prefixo das rotas de autenticação
+ * As rotas de autenticação são geradas automaticamente pelo next-auth
+ * @type {string}
+ */
+
+export const apiAuthPrefix = '/api/auth';
+
+/**
+ * Rota de redirecionamento ao concluir o login
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = '/settings';
