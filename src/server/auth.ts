c4ad8767077014b7293b678b6076a7907b69d071
Luis Dourado <lfaddourado@gmail.com>
2024-01-20 14:43:46 -0300

    refactor: extend session with user roles

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,7 +1,16 @@
-import NextAuth from 'next-auth';
+import NextAuth, { type DefaultSession } from 'next-auth';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { authDb, dbPromise } from './db';
 import authConfig from './auth.config';
+import { getUserById } from '@/lib/user';
+
+declare module 'next-auth' {
+	interface Session {
+		user: {
+			role: string | null;
+		} & DefaultSession['user'];
+	}
+}
 
 export const {
 	handlers: { GET, POST },
@@ -9,6 +18,30 @@ export const {
 	signIn,
 	signOut,
 } = NextAuth({
+	callbacks: {
+		async session({ session, token }) {
+			if (token.sub && session.user) {
+				session.user.id = token.sub;
+			}
+			if (token.role && session.user) {
+				session.user.role = token.role as 'ADMIN' | 'USER' | null;
+			}
+			console.log('session', { session });
+			console.log('token from session', { token });
+			return session;
+		},
+
+		async jwt({ token }) {
+			console.log('jwt token', { token });
+			if (!token.sub) return token;
+			const user = await getUserById(token.sub);
+			if (!user) return token;
+			console.log('user from jwt', { user });
+			token.role = user.role;
+
+			return token;
+		},
+	},
 	adapter: DrizzleAdapter(authDb),
 	session: {
 		strategy: 'jwt',
