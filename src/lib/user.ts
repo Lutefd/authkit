3398ac135875458a898562e5c1a205836f4b0c61
Luis Dourado <lfaddourado@gmail.com>
2024-01-20 20:36:29 -0300

    feat: add setDefaultRoleAndStatus, setUserRole, and setUserStatus functions

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -38,6 +38,51 @@ export const setDefaultRoleAndStatus = async (id: string) => {
 				role: 'USER',
 				status: 'ACTIVE',
 			})
+			.where(eq(users.id, id))
+			.returning({
+				role: users.role,
+				id: users.id,
+				name: users.name,
+				email: users.email,
+				image: users.image,
+				status: users.status,
+			});
+
+		return user;
+	} catch (error) {
+		return null;
+	}
+};
+
+export const setUserRole = async (id: string, role: 'ADMIN' | 'USER') => {
+	const db = await dbPromise;
+
+	try {
+		const user = await db
+			.update(users)
+			.set({
+				role,
+			})
+			.where(eq(users.id, id));
+
+		return user;
+	} catch (error) {
+		return null;
+	}
+};
+
+export const setUserStatus = async (
+	id: string,
+	status: 'ACTIVE' | 'BLOCKED'
+) => {
+	const db = await dbPromise;
+
+	try {
+		const user = await db
+			.update(users)
+			.set({
+				status,
+			})
 			.where(eq(users.id, id));
 
 		return user;
