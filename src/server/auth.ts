3dcd6e1d23dbf6de28edf6d4799d165f8670033f
Luis Dourado <lfaddourado@gmail.com>
2024-01-21 15:44:34 -0300

    refactor: update auth.ts file with new user event and page configurations

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -2,7 +2,7 @@ import NextAuth, { type DefaultSession } from 'next-auth';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { authDb, dbPromise } from './db';
 import authConfig from './auth.config';
-import { getUserById, setDefaultRoleAndStatus } from '@/lib/user';
+import { getUserById, setDefaultRoleStatusAndDate } from '@/lib/user';
 import { pgTable } from './db/schema';
 
 declare module 'next-auth' {
@@ -19,12 +19,18 @@ export const {
 	signIn,
 	signOut,
 } = NextAuth({
+	pages: {
+		signIn: '/auth/login',
+		error: '/auth/error',
+	},
+	events: {
+		async linkAccount({ user }) {
+			await setDefaultRoleStatusAndDate(user.id);
+		},
+	},
 	callbacks: {
 		async signIn({ user }) {
 			const existingUser = await getUserById(user.id);
-			if (existingUser?.status == null || existingUser?.role == null) {
-				await setDefaultRoleAndStatus(user.id);
-			}
 			if (existingUser?.status == 'BLOCKED') {
 				return false;
 			}
@@ -43,20 +49,11 @@ export const {
 		},
 
 		async jwt({ token, account }) {
-			if (account && account.access_token) {
-				if (!token.sub) return token;
-				const user = await getUserById(token.sub);
-				if (!user) return token;
-				if (user.status == null || user.role == null) {
-					console.log('setDefaultRoleAndStatus');
-					const updatedUser = await setDefaultRoleAndStatus(user.id);
-					if (!updatedUser) return token;
-					token.role = updatedUser[0].role;
-					console.log('token role', token.role);
-				} else {
-					token.role = user.role;
-				}
-			}
+			if (!token.sub) return token;
+			const user = await getUserById(token.sub);
+			if (!user) return token;
+
+			token.role = user.role;
 			return token;
 		},
 	},
