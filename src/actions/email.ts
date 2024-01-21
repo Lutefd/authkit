d2fdba18eaf39c593387da5784464774606c7aca
Luis Dourado <lfaddourado@gmail.com>
2024-01-21 17:30:30 -0300

    feat: add email functionality for sending verification emails

---
+++ src/actions/email.ts
@@ -0,0 +1,56 @@
+'use server';
+require('dotenv').config();
+
+import { env } from '@/env';
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({
+	apiVersion: 'latest',
+	region: 'sa-east-1',
+	credentials: {
+		accessKeyId: env.SES_ACCESS_KEY_ID!,
+		secretAccessKey: env.SES_SECRET_ACCESS_KEY!,
+	},
+});
+
+console.log(ses.config.credentials);
+
+export const sendEmail = async (to: string, subject: string, body: string) => {
+	const params = {
+		Destination: {
+			ToAddresses: [to],
+		},
+		Message: {
+			Body: {
+				Html: {
+					Charset: 'UTF-8',
+					Data: body,
+				},
+			},
+			Subject: {
+				Charset: 'UTF-8',
+				Data: subject,
+			},
+		},
+		Source: env.SES_EMAIL,
+	};
+
+	try {
+		const emailResult = await ses.sendEmail(params).promise();
+		console.log(emailResult);
+		return emailResult.MessageId;
+	} catch (error) {
+		console.log(error);
+		return error;
+	}
+};
+
+export const sendVerificationEmail = async (to: string, token: string) => {
+	const subject = 'Verifique seu email';
+	const body = `
+        <p>Obrigado por se Cadastrar!</p>
+        <p>Por favor verifique seu email clicando <a href="http://localhost:3000/auth/verify-email?token=${token}">aqui</a>.</p>
+    `;
+
+	return await sendEmail(to, subject, body);
+};
