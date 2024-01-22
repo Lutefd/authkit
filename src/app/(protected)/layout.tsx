0f61654a2159bd56eef7c52b0d541522992dc26e
Luis Dourado <lfaddourado@gmail.com>
2024-01-22 11:24:02 -0300

    refactor: add Navbar component to AuthLayout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,6 +2,7 @@ import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { auth } from '@/server/auth';
 import { redirect } from 'next/navigation';
 import SessionProvider from '@/components/providers/SessionProvider';
+import Navbar from './_components/Navbar';
 
 const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
 	const session = await auth();
@@ -9,9 +10,12 @@ const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
 		redirect('/auth/login');
 	}
 	return (
-		<div className="">
-			<SessionProvider session={session}>{children}</SessionProvider>
-		</div>
+		<SessionProvider session={session}>
+			<div className="h-full w-full flex flex-col gap-y-10 items-center justify-center  bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-purple-400 to bg-purple-800">
+				<Navbar />
+				{children}
+			</div>
+		</SessionProvider>
 	);
 };
 
