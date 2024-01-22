740e855e145c024e73acd6b7d9a7e99d962a371c
Luis Dourado <lfaddourado@gmail.com>
2024-01-22 10:50:36 -0300

    feat: add SessionProvider component to AuthLayout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,13 +1,18 @@
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { auth } from '@/server/auth';
 import { redirect } from 'next/navigation';
+import SessionProvider from '@/components/providers/SessionProvider';
 
 const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
 	const session = await auth();
 	if (!session) {
 		redirect('/auth/login');
 	}
-	return <div className="">{children}</div>;
+	return (
+		<div className="">
+			<SessionProvider session={session}>{children}</SessionProvider>
+		</div>
+	);
 };
 
 export default AuthLayout;
