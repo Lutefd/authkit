af9be52d3814f7334edd19ee1d68363df34c1293
Luis Dourado <lfaddourado@gmail.com>
2024-01-23 15:17:57 -0300

    refactor: add currentRole function to retrieve user's role

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -100,3 +100,8 @@ export const currentUserServer = async () => {
 	const session = await auth();
 	return session?.user;
 };
+
+export const currentRole = async () => {
+	const session = await auth();
+	return session?.user.role;
+};
